74697a0bef1e45dbbd33a98270bd0f01aa187eac
ishraqX <147395266+ishraqX@users.noreply.github.com>
2026-03-06 06:27:15 +0000

    remove combineCSS

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if (!isset($_SESSION[$session_key])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="assets/css/combined.min.css">
+    
     <link rel="stylesheet" href="assets/css/overflow-fix.css">
     
     
